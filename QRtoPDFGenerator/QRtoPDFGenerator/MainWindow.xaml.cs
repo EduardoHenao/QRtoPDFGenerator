1e21d7c276c33ada0bd0d7ba547eebb7d7909257
Eduardo Henao <eduardo.henao@outlook.com>
2019-09-17 14:02:37 -0500

    code calculation methods and add variables
    cable calculation method to all events

--- QRtoPDFGenerator/QRtoPDFGenerator/MainWindow.xaml.cs
+++ QRtoPDFGenerator/QRtoPDFGenerator/MainWindow.xaml.cs
@@ -1,58 +1,105 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-
-namespace QRtoPDFGenerator
+﻿namespace QRtoPDFGenerator
 {
+    using System.Windows;
+    using System.Windows.Controls;
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+        // algorithm variables
+        private static string SERIES = "AAA";
+        private static int QUANTITY = 1;
+        private static int STARTING_POINT = 0;
+        private static int DIGITS = 3;
+        private static string SEPARATOR = "-";
+
+
+        // preview variables
+        private string resultStart = SERIES + SEPARATOR + STARTING_POINT.ToString("D" + DIGITS);
+        private string resultEnd = string.Empty;
+
+        // wpf variables
+        private bool endVisible = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            this.InitializeAlgoVariables();
+        }
+
+        private void InitializeAlgoVariables()
+        {
+            this.DigitsTextBox.Text = DIGITS.ToString();
+            this.QuantityTextBox.Text = QUANTITY.ToString();
+            this.StartingNumberTextBox.Text = STARTING_POINT.ToString();
+            this.DigitsTextBox.Text = DIGITS.ToString();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            //TODO: after having all the events coded
+        }
 
+        private void RecalculatePreview()
+        {
+            this.resultStart = this.GenerateCode(0);
+            this.resultEnd = this.GenerateCode(QUANTITY - 1);
+            this.endVisible = QUANTITY > 1;
+            this.setVisibilityWithQuantity();
+        }
+
+        private string GenerateCode(int offset)
+        {
+            string digit = (STARTING_POINT + offset).ToString("D" + DIGITS);
+            if (string.IsNullOrEmpty(SERIES)) return digit;
+            return SERIES + SEPARATOR + digit;
+        }
+
+        private void setVisibilityWithQuantity()
+        {
+            if (this.ResultadoDesdeText != null && this.ResultadoHastaText != null)
+            {
+                this.ResultadoDesdeText.Text = this.resultStart;
+                this.ResultadoHastaText.Text = this.resultEnd;
+
+                if (this.endVisible)
+                {
+                    this.ResultadoHastaText.Visibility = Visibility.Visible;
+                    this.ResultadoHastaTitle.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    this.ResultadoHastaText.Visibility = Visibility.Hidden;
+                    this.ResultadoHastaTitle.Visibility = Visibility.Hidden;
+                }
+            }
         }
 
         private void DigitsChanged(object sender, TextChangedEventArgs e)
         {
-
+            this.RecalculatePreview();
         }
 
         private void SeriesChanged(object sender, TextChangedEventArgs e)
         {
-
+            this.RecalculatePreview();
         }
 
         private void StartingPointChanged(object sender, TextChangedEventArgs e)
         {
-
+            this.RecalculatePreview();
         }
 
         private void QuantityChanged(object sender, TextChangedEventArgs e)
         {
-
+            this.RecalculatePreview();
         }
 
         private void SeparatorChanged(object sender, TextChangedEventArgs e)
         {
-
+            this.RecalculatePreview();
         }
     }
 }
